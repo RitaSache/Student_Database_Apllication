3fac31e2a9ffd83854a561cbe873ece5f2240dd3
Rita <margarita7017@gmail.com>
2018-03-09 00:05:23 -0800

    update comments

--- src/DbConfig.java
+++ src/DbConfig.java
@@ -9,7 +9,7 @@ public class DbConfig {
             //returns the Class object associated with the class or interface with the given string name
             Class.forName("com.mysql.jdbc.Driver");
             String connectionUrl = "jdbc:mysql://localhost:3306/StudentDB"; //could be ip address or hostname
-            mysqlConnection = DriverManager.getConnection(connectionUrl, "root", ""); //I purposefully did not set the password for easier access
+            mysqlConnection = DriverManager.getConnection(connectionUrl, "root", "");
 
         } catch (Exception ex) {
             ex.printStackTrace();
